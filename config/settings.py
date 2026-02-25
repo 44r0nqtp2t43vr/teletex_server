0b01cc4b67defe35f445e59bca34d47a815eca57
frankaong <belleisaong@gmail.com>
2026-02-25 16:24:51 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 FIREBASE_CRED_PATH = BASE_DIR / "firebase/firebase-service-account.json"
 FIREBASE_STORAGE_BUCKET = "teletex-tt.firebasestorage.app"
